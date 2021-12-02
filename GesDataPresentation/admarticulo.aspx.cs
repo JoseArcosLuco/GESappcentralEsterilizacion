27326a86056842078783202aa599782b308f3ec0
Jose Arcos <jose.arcos.luco@gmail.com>
2021-12-02 20:01:39 -0300

    estados en las grillas y botones de activacion

--- GesDataPresentation/admarticulo.aspx.cs
+++ GesDataPresentation/admarticulo.aspx.cs
@@ -59,11 +59,13 @@ namespace ges.data.presentation
                     foreach (Articulo pdto in obj.ListArticulo)
                     {
                         string identificador = pdto.idArticulo.ToString();
-                        string cadest = "Inactivo";
+                        string cadest = "Activar";
+                        string cadestDesc = "Desactivo";
                         string cadclass = "glyphicon-remove";
                         if (pdto.estado == 1)
                         {
-                            cadest = "Activo";
+                            cadest = "Desactivar";
+                            cadestDesc = "Activo";
                             cadclass = "glyphicon-ok";
                         }
                         tbl.Append("<tr id=\"fila" + identificador + "\">");
@@ -73,7 +75,7 @@ namespace ges.data.presentation
                         tbl.Append("<td>" + pdto.codigoArticulo.ToString() + "</td>");
                         tbl.Append("<td>" + pdto.codigoBarra.ToString() + "</td>");
 
-                        tbl.Append("<td id=\"valestado_" + identificador + "\" rel=\"" + pdto.estado + "\">" + cadest + "</td>");
+                        tbl.Append("<td id=\"valestado_" + identificador + "\" rel=\"" + pdto.estado + "\">" + cadestDesc + "</td>");
                         tbl.Append("<td class=\"text-right\">");
                         //tbl.Append(" <button type=\"button\" id=\"btnEliminar_" + identificador + "\" class=\"btn btn-primary btn-fw\" aria-label=\"Left Align\" title=\"Eliminar\" onclick=\"javascript:Eliminar(" + identificador + ")\"><i class=\"mdi mdi-alert-outline\"></i>Eliminar</button>");
                         //tbl.Append(" &nbsp;<button type=\"button\" id=\"btnEditar_" + identificador + "\" class=\"btn btn-info\" aria-label=\"Left Align\" title=\"Editar\" onclick=\"javascript:Editar(" + identificador + ")\"><span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span></button>");
