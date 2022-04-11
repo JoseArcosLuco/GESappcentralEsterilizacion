bc50bd67df032efe1ebd56e57cba51a34999a3a7
Christian <christian.santana.j@gmail.com>
2022-04-11 14:14:30 -0400

    Se agrega la clase de ejemplo carro

---
+++ GesDataModel/Carro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ges.data.model
+{
+    public class Carro
+    {
+        public Int32 idCarro { get; set; }
+        public string nombre { get; set; }
+        public string fechaReg { get; set; }
+        public string fechaAct { get; set; }
+        public int estado { get; set; }
+    }
+    public partial class listaCarro
+    {
+        public BindingList<Carro> ListCarro = new BindingList<Carro>();
+        public listaCarro()
+        {
+        }
+        public void Add(Carro value)
+        {
+            this.ListCarro.Add(value);
+        }
+    }
+}
